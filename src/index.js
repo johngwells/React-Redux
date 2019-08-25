19158ef76ff3e2fe609eea3d64ac22e9faa7426c
John Wells <molekewl2017@gmail.com>
2019-08-25 02:28:19 -0700

    mapStateToProps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import rootReducer from './reducers';
@@ -18,8 +19,11 @@ store.subscribe(() => console.log('store.getState()', store.getState()));
 // store.dispatch(cancelGame());
 // store.dispatch(collapseInstructions());
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+    document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
