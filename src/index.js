0effbd9dc12729490250c5390dc89c9621b802cd
John Wells <molekewl2017@gmail.com>
2019-08-25 02:03:30 -0700

    refactor split code in their own folders/files

--- src/index.js
+++ src/index.js
@@ -3,61 +3,20 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import './index.css';
 import App from './App';
+import rootReducer from './reducers';
 import * as serviceWorker from './serviceWorker';
-
-const DEFAULT_SETTINGS = {
-  gameStarted: false,
-  instructionsExpanded: false
-}
-
-const SET_GAME_STARTED = 'SET_GAME_STARTED';
-const SET_INSTRUCTIONS_EXPANDED = 'SET_INSTRUCTIONS_EXPANDED'
-
-const rootReducer = (state = DEFAULT_SETTINGS, action) => {
-  console.log('state', state, 'action', action);
-  
-  switch(action.type) {
-    case SET_GAME_STARTED:
-      return { ...state, instructionsExpanded: state.instructionsExpanded };
-    case SET_INSTRUCTIONS_EXPANDED:
-      return { ...state, instructionsExpanded: action.instructionsExpanded };
-    default:
-      return state;
-  }
-};
 
 const store = createStore(rootReducer);
 
 console.log('store', store);
+console.log('store.getState()', store.getState());
 
 store.subscribe(() => console.log('store.getState()', store.getState()));
 
-const startGame = () => {
-  return {
-    type: SET_GAME_STARTED,
-    gameStarted: true
-  };
-}
-
-const cancelGame = () => {
-  return {
-    type: SET_GAME_STARTED,
-    gameStarted: false
-  };
-}
-
-const expandInstructions = () => {
-  return { type: SET_INSTRUCTIONS_EXPANDED, instructionsExpanded: true };
-}
-
-const cancelInstructions = () => {
-  return { type: SET_INSTRUCTIONS_EXPANDED, instructionsExpanded: false };
-}
-
-store.dispatch(startGame());
-store.dispatch(expandInstructions());
-store.dispatch(cancelGame());
-store.dispatch(cancelInstructions());
+// store.dispatch(startGame());
+// store.dispatch(expandInstructions());
+// store.dispatch(cancelGame());
+// store.dispatch(collapseInstructions());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
